bee569140cd4f2fd563e20b7a54e2a009258255c
lilirunjie0@gmail.comm <lilirunjie0@gmail.com>
2026-03-26 17:53:20 +1100

    fix cors

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
+const PORT = 3000;
+
+
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.static(__dirname));
